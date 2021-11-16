f4905ae3ac611cb6737237f074be139fd545d247
Rached Khemakhem <rached.khemakhem@esprit.tn>
2021-11-16 12:57:45 +0100

    racheds

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:8-jdk-alpine
 EXPOSE 8083
-ADD target/timesheet-1.0.4-RELEASE timesheet-1.0.4-RELEASE
-ENTRYPOINT ["java","-jar","/timesheet-1.0.4-RELEASE"]
+ADD target/timesheet-1.5.4-SNAPSHOT timesheet-1.5.4-SNAPSHOT
+ENTRYPOINT ["java","-jar","/timesheet-1.5.4-SNAPSHOT"]
--- src/test/java/tn/esprit/spring/TimesheetTestApp.java
+++ src/test/java/tn/esprit/spring/TimesheetTestApp.java
@@ -57,45 +57,7 @@ private static final Logger l = LogManager.getLogger(TimesheetTestApp.class);
 	   
 	  
 	  
-	 /*
-	   @Test
-	   public void ajouterTimesheet() throws Exception {
-	      String uri = "/ajouterTimesheet/1/1/12-12-2021/12-12-2022";
-	      Mission mission = new Mission();
-	      mission.setId(2);
-	      mission.setName("Ginger");
-	      mission.setDescription("mission description");
-	      Employe employe = new Employe();
-	      employe.setId(2);;
-	      employe.setNom("Bob");
-	      employe.setPrenom("DART");
-	      employe.setEmail("employe@esprit.tn");
-	      employe.setPassword("bobbypassword");
-	      employe.setRole(Role.INGENIEUR);
-	      TimesheetPK tspk = new TimesheetPK();
-	      Timesheet timesheet = new Timesheet();
-	      timesheet.setValide(true);
-	      timesheet.setMission(mission);
-	      timesheet.setTimesheetPK(tspk);
-	      timesheet.setEmploye(employe);
-	 
-	      String inputJson = super.mapToJson(timesheet);
-	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
-	         .contentType(MediaType.APPLICATION_JSON_VALUE)
-	         .content(inputJson)).andReturn();
-	      
-	      int status = mvcResult.getResponse().getStatus();
-	      assertEquals(200, status);
-	      if(status == 200) {
-	    	  l.info("le timesheet a été ajouté avec succés");
-		      }else if(status >= 400 && status <500) {
-		    	  l.fatal("erreur coté client :  code reponse :  "+status);
-		      }else if(status >= 500){
-		    	  l.error("erreur coté serveur :  code reponse :  "+status);
-		      }
-	   }
-	   */
-	   
+	
 	 
 	   @Test
 	   public void getAllEmployeByMission() throws Exception {
